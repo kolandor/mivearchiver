1dcfec56fe2e1395f0c8ed9a1adaaaaa18bd2f6f
nick.nameless <nickjtt357@gmail.com>
2018-07-21 19:51:59 +0300

    Added some features(progress bar and cancelling)

--- MiveArchiver/IArchiver.cs
+++ MiveArchiver/IArchiver.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MiveArchiver.ThreadCompressFileData;
 
 namespace MiveArchiver
 {
     interface IArchiver
     {
-        void Compress(string sourceFile, string compressedFile, ProgressBar bar);
+        void CancelWork();
 
-        void Decompress(string compressedFile, string targetFile, ProgressBar bar);
+        void Compress(string sourceFile, string compressedFile, ProgressSet progressSet);
+
+        void Decompress(string compressedFile, string targetFile, ProgressSet progressSet);
     }
 }
---
+++ MiveArchiver/ThreadCompressFileData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiveArchiver
+{
+    public class ThreadCompressFileData
+    {
+        public delegate void ProgressSet(long valueToUp);
+
+        public string FileFrom { get; set; }
+
+        public string FileTo { get; set; }
+
+        public ProgressSet CompressProgressSet { get; set; }
+
+    }
+}
